04c3eb9d48b677e5dc532b0917a60d131c78dd8d
Sima Margulis Davidi <simamarg@gmail.com>
2017-12-24 17:26:39 +0200

    rewrite form validation to have one message box

--- script.js
+++ script.js
@@ -38,51 +38,43 @@ var submitForm = function() {
     var repPass = document.getElementById("rep-password").value;
     var country = document.getElementById("country").value;
     var phone = document.getElementById("phone").value;
-    var userElement = document.getElementById("user-message");
-    var passElement =  document.getElementById("pass-message");
-    var repeatPassElement = document.getElementById("rep-pass-message");
-    var countryElement = document.getElementById("country-message");
-    var phoneElement = document.getElementById("phone-message");
+
+    var messageElement = document.getElementById("message-box");
 
     var correctInput = true;
 
+    messageElement.innerHTML = "";
+    messageElement.setAttribute("style", "color: red;");
+
     if (user.length < 5) {
-        userElement.innerHTML = "The supplied username is too short, try again using 5 or more characters<br>";
+        messageElement.innerHTML += "The supplied username is too short, try again using 5 or more characters<br>";
         correctInput = false;
     } else {
         alert("Thank you " + user);
-        userElement.innerHTML = "";
     }
     
     if (pass.length < 6) {
-        passElement.innerHTML = "The supplied password is too short, try again using 6 or more characters<br>";
+        messageElement.innerHTML += "The supplied password is too short, try again using 6 or more characters<br>";
         correctInput = false;
-    } else {
-        passElement.innerHTML = "";
     }
 
     if (pass !== repPass) {
-        repeatPassElement.innerHTML = "The supplied passwords don't match, try again<br>";
+        messageElement.innerHTML += "The supplied passwords don't match, try again<br>";
         correctInput = false;
-    } else {
-        repeatPassElement.innerHTML = "";
     }
 
     if (!(country.toLowerCase() === "usa" || country.toLowerCase() === "israel")) {
-        countryElement.innerHTML = "The supplied country is invalid (valid countries are Israel/USA)<br>"
+        messageElement.innerHTML += "The supplied country is invalid (valid countries are Israel/USA)<br>";
         correctInput = false;
-    } else {
-        countryElement.innerHTML = "";
     }
 
     if (phone.length < 15) {
-        phoneElement.innerHTML = "The supplied phone number is too short, try again using 15 or more digits<br>"
+        messageElement.innerHTML += "The supplied phone number is too short, try again using 15 or more digits<br>";
         correctInput = false;
-    } else {
-        phoneElement.innerHTML = "";
     }
 
     if (correctInput) {
-        document.getElementById("correct-input").innerHTML = "Well done " + user + "! All your input is correct :)"; 
+        messageElement.setAttribute("style", "color: green;")
+        messageElement.innerHTML = "Well done " + user + "! All your input is correct :)";
     }
 };
